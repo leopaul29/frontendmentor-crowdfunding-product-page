37c71e1cedd1f1799f08db7f3a184e78de4606bd
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-13 23:56:38 +0100

    add nav component

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+        <li>Discover</li>
+        <li>Get Started</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
