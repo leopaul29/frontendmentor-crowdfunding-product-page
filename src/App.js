f8f9b6cd2cdc2606ef927ab28612b2bd96ecc904
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-14 00:37:14 +0100

    add stats component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Stats from "./components/Stats";
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
       <Header/>
+      <Stats/>
     </div>
   );
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,9 @@
+export default function Stats() {
+  return (
+    <div className="stats">
+      <div className="stats__totalBacked">$89,914 of $100,000 backed</div>
+      <div className="stats__totalBackers">5,007 total backers</div>
+      <div className="stats__dasLeft">56 days left</div>
+    </div>
+  );
+}
