1c851bd501dd6be0d2c3f81e37d8da1a52f658f5
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-14 00:38:09 +0100

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <div className="header__title">Mastercraft Bamboo Monitor Riser</div>
+      <div className="header__subtitle">
+        A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+      </div>
+      <div className="header__cta button">Back this project</div>
+      <div className="header__bookmark">Bookmark</div>
+    </header>
+  );
+}
