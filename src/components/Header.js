a9e563dc44b3eb2a4b13680f8a1b379a0c6d3add
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-15 23:04:32 +0100

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,18 @@ export default function Header(props) {
   const { title, subtitle, mainButton, bookmarkButton } = props;
 
   return (
-    <header className="header">
-      <div className="header__logo"></div>
-      <div className="header__title">{title}</div>
-      <div className="header__subtitle">{subtitle}</div>
-      <div className="header__cta button">Back this project</div>
-      <div className="header__bookmark">Bookmark</div>
+    <header className="header tile">
+      <div className="header__logo">
+        <img src="./images/logo-mastercraft.svg" alt="logo-mastercraft" />
+      </div>
+      <div className="header__content">
+        <h1 className="header__title">{title}</h1>
+        <h3 className="header__subtitle">{subtitle}</h3>
+        <div className="header__actions">
+          <div className="header__cta btn btn-action">Back this project</div>
+          <div className="header__bookmark btn">Bookmark</div>
+        </div>
+      </div>
     </header>
   );
 }
