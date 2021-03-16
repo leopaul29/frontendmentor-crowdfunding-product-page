2c8a6b2e6d1578c00c114c65eb38840120233680
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-16 21:43:39 +0100

    style project

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,12 +4,10 @@ import Card from "./Card";
 export default function Project(props) {
   const { title, aboutP1, aboutP2, cards } = props;
   return (
-    <div className="project">
-      <div className="project__title">{title}</div>
-      <div className="project__about">
-        <p>{aboutP1}</p>
-        <p>{aboutP2}</p>
-      </div>
+    <div className="project tile">
+      <h2 className="project__title title">{title}</h2>
+      <p className="text">{aboutP1}</p>
+      <p className="text">{aboutP2}</p>
       <div className="project__cards">
         {cards.map((card) => (
           <Card key={card.key} {...card} />
