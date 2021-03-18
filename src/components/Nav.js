16466b825bd756c9dce87f5e0fea2b0876b7e0e9
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-18 22:58:03 +0100

    fix nav style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,22 @@ import "../styles/Nav.css";
 export default function Nav() {
   return (
     <nav className="nav">
-      <img src="/images/image-hero-desktop.jpg" alt="hero" />
-      <div className="nav__right">
-        <div className="nav__logo">crowdfund</div>
-      </div>
-      <div className="nav__left">
-        <ul className="nav__menu">
-          <li className="menu__item">About</li>
-          <li className="menu__item">Discover</li>
-          <li className="menu__item">Get Started</li>
-        </ul>
+      <img
+        src="/images/image-hero-desktop.jpg"
+        alt="hero"
+        className="nav__background"
+      />
+      <div className="nav__navbar">
+        <div className="nav__right">
+          <div className="nav__logo">crowdfund</div>
+        </div>
+        <div className="nav__left">
+          <ul className="nav__menu">
+            <li className="menu__item">About</li>
+            <li className="menu__item">Discover</li>
+            <li className="menu__item">Get Started</li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
