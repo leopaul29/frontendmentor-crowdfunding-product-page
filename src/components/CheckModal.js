c526d289e04545341a8cb8786fbbe27bea7e48cc
Leo-paul <leopaulmartin@hotmail.fr>
2021-03-23 23:11:16 +0100

    style: checkmodal

---
+++ src/components/CheckModal.js
@@ -0,0 +1,20 @@
+import "../styles/CheckModal.css";
+
+export default function CheckModal() {
+  return (
+    <div className="checkModal tile">
+      <div className="checkModal__icon">
+        <img src="./images/icon-check.svg" alt="icon-check" />
+      </div>
+      <div className="modalCard__title card__title title">
+        Thanks for your support!
+      </div>
+      <div className="card__about checkModal__about">
+        Your pledge brings us one step closer to sharing Mastercraft Bamboo
+        Monitor Riser worldwide. You will get an email once our campaign is
+        completed.
+      </div>
+      <div className="card__button button btn btn-primary">Got it!</div>
+    </div>
+  );
+}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "../styles/Project.css";
 import Card from "./Card";
+import CheckModal from "./CheckModal";
 import Modal from "./Modal";
 
 export default function Project(props) {
   const { title, aboutP1, aboutP2, cards } = props;
 
   const [toggleModal, setToggleModal] = useState(false);
+  const [toggleCheckModal, setToggleCheckModal] = useState(false);
 
   const btntoggleModal = () => {
     setToggleModal(!toggleModal);
@@ -14,18 +16,26 @@ export default function Project(props) {
   const off = () => {
     setToggleModal(false);
   };
+  const btntoggleCheckModal = () => {
+    setToggleCheckModal(!toggleCheckModal);
+  };
 
   return (
     <div className="project tile">
       <h2 className="project__title title">{title}</h2>
       <p className="text">{aboutP1}</p>
       <p className="text">{aboutP2}</p>
-      <button onClick={btntoggleModal}>test</button>
-      <div
-        class={"overlay " + (toggleModal ? "modal-block" : "modal-none")}
-      >
+      <button onClick={btntoggleModal}>modal</button>
+      <button onClick={btntoggleCheckModal}>check-modal</button>
+      <div class={"overlay " + (toggleModal ? "modal-block" : "modal-none")}>
         <div class="overlay__content">
           <Modal />
+        </div>
+      </div>
+      
+      <div class={"overlay " + (toggleCheckModal ? "modal-block" : "modal-none")}>
+        <div class="overlay__content">
+          <CheckModal />
         </div>
       </div>
 
